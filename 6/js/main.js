fe374762b43fccec14a5160941b4de1b7e672d6a
Keks <keks@htmlacademy.ru>
2026-01-16 02:16:41 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,9 @@
+import { createPhotosArray } from './data.js';
+
+// Создаем массив фотографий
+window.photosArray = createPhotosArray();
+
+import { initPictureThumbnails } from './picture-thumbnails.js';
+
+//Отрисовка миниатюр
+initPictureThumbnails();
